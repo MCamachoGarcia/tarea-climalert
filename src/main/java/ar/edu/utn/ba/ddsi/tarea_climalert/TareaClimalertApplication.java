5c46a9a8786fade8c1d47b121b4ce4afecd1b538
Mauricio Camacho <mcamachogarcia@frba.utn.edu.ar>
2026-07-05 17:49:09 -0300

    Estructura proyecto desde Spring Initializr

---
+++ src/main/java/ar/edu/utn/ba/ddsi/tarea_climalert/TareaClimalertApplication.java
@@ -0,0 +1,13 @@
+package ar.edu.utn.ba.ddsi.tarea_climalert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TareaClimalertApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TareaClimalertApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ar/edu/utn/ba/ddsi/tarea_climalert/TareaClimalertApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.utn.ba.ddsi.tarea_climalert;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TareaClimalertApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
